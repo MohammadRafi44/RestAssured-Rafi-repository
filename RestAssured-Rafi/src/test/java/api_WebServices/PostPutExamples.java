67612f8c0a14ae54aa225c8160c2f2bd630fcb46
PKunapareddy <PKunapareddy@elm.sa>
2021-03-15 16:03:23 +0300

    # WARNING: head commit changed in the meantime
    commit march 15

---
+++ RestAssured-Rafi/src/test/java/api_WebServices/PostPutExamples.java
@@ -0,0 +1,149 @@
+package api_WebServices;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+
+
+/*
+ * POST -- Creates new Resource
+ * 
+ * Put  -- Updates the existing Resource -- by passing all the nodes
+ * 
+ * Patch -- Udpates existing Resource -- by passing specific node
+ */
+
+public class PostPutExamples {
+
+	@Test
+	public void test_get01() {
+		
+		/*
+		 * baseURI -- is a method coming from RestAssured -- to set baseurl -- eg :- http://localhost
+		 */
+		baseURI = "http://localhost:3000/";  
+		
+		given().
+		get("/users"). // setting URI path 
+		then().
+		statusCode(200).
+		log().all();
+	}
+	
+	
+	
+	@Test
+	public void test_get02() {
+		baseURI = "http://localhost:3000/";
+		/*
+		 * This is equal to calling "http://localhost:3000/subjects?name=Automation"
+		 */
+		given().param("name", "Automation").
+		get("/subjects").  // URI Path
+		then().
+		statusCode(200).
+		log().all();
+	}
+	
+	
+	@Test
+	public void test_post() {
+		
+		/*
+		 * POST is always for creating a resource ( does not matter if it was duplicated )
+		 */
+		
+		JSONObject request = new JSONObject();
+		request.put("firstName", "Tom");
+		request.put("lastname", "Moody");
+		request.put("subjectid", 1);
+		
+		
+		baseURI = "http://localhost:3000/";
+		
+		
+		given().
+			contentType(ContentType.JSON).accept(ContentType.JSON).
+			header("Content-Type","application/json").
+			body(request.toJSONString()).
+		when().
+			post("/users").
+		then().
+			statusCode(201).
+			log().all();
+		
+	
+	}
+	
+	@Test
+	public void test_patch() {
+		
+		/*
+		 * PATCH is always for update a resource -- by passing specific node
+		 */
+		
+		JSONObject request = new JSONObject();
+		
+		request.put("lastname", "Cooper1");
+		
+		
+		
+		baseURI = "http://localhost:3000/";
+		
+		
+		given().
+			contentType(ContentType.JSON).accept(ContentType.JSON).
+			header("Content-Type","application/json").
+			body(request.toJSONString()).
+		when().
+			patch("/users/4").
+		then().
+			statusCode(200).
+			log().all();
+		
+	
+	}
+	
+	@Test
+	public void test_put() {
+		
+		/*
+		 * PUT is for checking if resource is exists then update , else create new resource
+		 * 
+		 * For PUT -- we have to pass complete resource
+		 */
+		
+		JSONObject request = new JSONObject();
+		request.put("firstName", "Marry");
+		request.put("lastname", "Jane1");
+		request.put("subjectid", 1);
+		
+		
+		baseURI = "http://localhost:3000/";
+		
+		
+		given().
+			contentType(ContentType.JSON).accept(ContentType.JSON).
+			header("Content-Type","application/json").
+			body(request.toJSONString()).
+		when().
+			put("/users/4").
+		then().
+			statusCode(200).
+			log().all();
+		
+	
+	}
+	
+	@Test
+	public void test_delete() {
+		baseURI = "http://localhost:3000/";
+		
+			delete("/users/4").
+		then().
+			statusCode(200);
+	}
+}
